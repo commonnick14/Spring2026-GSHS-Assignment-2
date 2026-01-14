938d40452249d7e76dea229a8e2c7e8a93923958
commonnick14 <kim01093351216@gmail.com>
2026-01-14 17:47:36 +0900

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
 def logistic_regression(x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray) -> np.ndarray:
     '''
@@ -6,13 +7,20 @@ def logistic_regression(x_train: np.ndarray, y_train: np.ndarray, x_test: np.nda
 
     Parameters:
         - x_train: Training features of shape (n_samples, 2).
-                    For this assignment, each training sample has two features: [feature1, feature2]
         - y_train: Training labels (0/1)
-                    All the predictions will be binary (0 or 1), since it is Logistic Regression.
         - x_test: Test features of shape (n_samples, 2).
 
     Returns:
         y_pred: Predicted labels for the test set
     '''
-    # Your code here
-    pass
+
+    # Logistic Regression model
+    model = LogisticRegression()
+
+    # Train the model
+    model.fit(x_train, y_train)
+
+    # Predict labels for test data
+    y_pred = model.predict(x_test)
+
+    return y_pred
